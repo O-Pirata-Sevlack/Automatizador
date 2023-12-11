ca9a4d67531d22250d5b1bed635a0f8763b0485e
Calebe Santos <calebesantos@hotmail.com>
2023-12-11 15:59:58 -0300

    Front Ok

--- main.py
+++ main.py
@@ -16,17 +16,17 @@ contador = 1
 def index():
     if request.method == 'POST':
         Genero_R = request.form['P1']
-        Nome_R = request.form['P1']
-        Cargo_R = request.form['P1']
-        Órgão_R = request.form['P1']
-        Endereço_R = request.form['P1']
-        CEP_R = request.form['P1']
-        Cidade_R = request.form['P1']
-        Assunto = request.form['P1']
-        Nome_E = request.form['P1']
-        Genero_E = request.form['P1']
-        Cargo_E = request.form['P1']
-        Texto = request.form['P1']
+        Nome_R = request.form['P2']
+        Cargo_R = request.form['P3']
+        Órgão_R = request.form['P4']
+        Endereço_R = request.form['P5']
+        Cep_R = request.form['P6']
+        Cidade_R = request.form['P7']
+        Nome_E = request.form['P8']
+        Genero_E = request.form['P9']
+        Cargo_E = request.form['P10']
+        Assunto = request.form['P11']
+        Texto = request.form['P12']
         dia = str(datetime.now().day)
         mês = datetime.now()
         mês = mês.strftime('%B')
@@ -38,7 +38,7 @@ def index():
         "Cargo_R" : Cargo_R,
         "Órgão_R" : Órgão_R,
         "Endereço_R" : Endereço_R,
-        "CEP_R" : CEP_R,   
+        "CEP_R" : Cep_R,   
         "Cidade_R" : Cidade_R,
         "dia" : dia,
         "mês" : mês,
